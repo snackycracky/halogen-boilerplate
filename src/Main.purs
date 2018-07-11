37710e3159b4c1e5aa7e2b9d2481573a55d037cd
Nils Petersohn <nils.petersohn@tedd.berlin>
2018-07-11 14:29:35 +0200

    wip

--- src/Main.purs
+++ src/Main.purs
@@ -2,13 +2,12 @@ module Main where
 
 import Prelude
 
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Class (liftEff)
+import Effect (Effect, liftEff)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
 import Layouts.Main (component, matchRoutes)
 
-main :: forall eff . Eff (HA.HalogenEffects eff) Unit
+main :: Effect Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
   app <- runUI component unit body
